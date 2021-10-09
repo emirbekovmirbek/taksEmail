d9e35040200f42c0d2ceb061190022758e142be0
Mirbek <83970336+emirbekovmirbek@users.noreply.github.com>
2021-10-09 21:48:28 +0600

    feat: add function submitHandler to check email

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,28 +1,23 @@
 import * as React from 'react';
 import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
 import Stack from '@mui/material/Stack';
 export default function Alerts({alerts}) {
     return (
         <Stack sx={{
-            maxWidth: "500px",
             width: "100%",
             position:"absolute",
-            bottom: 0,
+            top: 0,
             left: alerts.error || alerts.success? "50%": "-1000000px",
             transform: "translateX(-50%)",
-            transition: "all .8s" ,
         }}
                spacing={2} >
             {alerts.error &&
             <Alert severity="error">
-                <AlertTitle>Ошибка</AlertTitle>
-                {alerts.error}
+                             Ошибка: { alerts.error}
             </Alert>
             }
             {
                 alerts.success && <Alert severity="success">
-                    <AlertTitle>Успех</AlertTitle>
                     {alerts.success}
                 </Alert>
             }
--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import css from "./autocomplete.module.css";
 import Alerts from "./Alert";
+import axios from "axios";
 
 const Autocomplete = () => {
     //  Value - значение инпута
@@ -11,6 +12,8 @@ const Autocomplete = () => {
         error: "",
         success: "",
     });
+    // emailValidation -  регулярное выражение для проверки почты
+    const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     // domains - почтовые домены
     const domains = ["yahoo.com", "gmail.com", "google.com",
         "hotmail.com", "me.com", "aol.com", "mac.com", "live.com",
@@ -30,52 +33,76 @@ const Autocomplete = () => {
         }
         return inputDomainWS.length === result;
     }
-    // handleChange - фунция делает управляемым инпут
+    // handleChange - фунция для управления инпутом
     const changeHandler = (e) => {
         setValue(e.target.value);
+        if (value.trim()) {
+            setAlerts({ error: "", success: "",});
+        }
     }
     // handleClick - функция заполнение поля инпута текстовым знчением автокомплита
     const handleClick = (e) => {
         setValue(e.target.textContent);
         setIsOpen(false);
     }
-
+    // submitHandler - фунция
     const submitHandler = (e) => {
         e.preventDefault();
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!re.test(value.toLowerCase())) {
-            setAlerts((old) => {return {...old, error: "Некоректная почта"}});
-        }else {
+        if (!value.trim()) {
+            setAlerts((old) => {
+                return {...old, error: "Поле не может быть пустым"}
+            });
+        } else if (!emailValidation.test(value.toLowerCase())) {
+            setAlerts((old) => {
+                return {...old, error: "Некоректная почта"}
+            });
+        } else {
+            axios.get(`https://api.kickbox.com/v2/verify?email=${value}&apikey=live_d5bf2cc04a0c24efcc7056727f487e08aff43981a575ac7ba55aadb3be5aecec`)
+                .then((res) => {
+                    if (res.status === 200) {
+                        if (res.data.result === "deliverable") {
+                            setAlerts({ success: "Почта существует", error: ""});
+                        } else {
+                            setAlerts({success: "", error: "Такой почты нет или она не активна"});
+                        }
+                    }
 
+                })
+                .catch(() => {
 
+                })
         }
 
     }
     return (
         <div className={css.autocomplete}>
             <form className={css.autocomplete__form} onSubmit={submitHandler}>
-                <input type="text" className={css.email}
-                       value={value}
-                       onChange={changeHandler}
-                       placeholder="Ваш email"
-                       onClick={() => setIsOpen(true)}
-                       style={{boxShadow: alerts.error? "0px 2px 8px 2px rgba(255,31,11,0.45)":"none"}}
-                />
-                <ul className={css.domains} onMouseLeave={() => {
-                    setIsOpen(false)
-                }}>
-                    {(value.indexOf("@") !== -1 && isOpen) && domains.filter((option) => {
-                        const inputDomain = value.split("@")[1];
-                        return checkingDomain(inputDomain, option);
-                    }).map((option, index) => {
-                        const name = value.split("@")[0];
-                        return <li key={index}
-                                   className={css.domain} onClick={handleClick}>
-                            {name.split(" ").join("") + "@" + option}
-                        </li>
-                    })}
-                </ul>
-                <button>Поверка почты</button>
+                <h1 className={css.title}>Проверка почты</h1>
+                <div>
+
+                    <input type="text" className={css.email}
+                           value={value}
+                           onChange={changeHandler}
+                           placeholder="Напишите вашу почту"
+                           onClick={() => setIsOpen(true)}
+                           style={{boxShadow: alerts.error ? "0px 2px 8px 2px rgba(255,31,11,0.45)" : "none"}}
+                    />
+                    <ul className={css.domains} onMouseLeave={() => {
+                        setIsOpen(false)
+                    }}>
+                        {(value.indexOf("@") !== -1 && isOpen) && domains.filter((option) => {
+                            const inputDomain = value.split("@")[1];
+                            return checkingDomain(inputDomain, option);
+                        }).map((option, index) => {
+                            const name = value.split("@")[0];
+                            return <li key={index}
+                                       className={css.domain} onClick={handleClick}>
+                                {name.split(" ").join("") + "@" + option}
+                            </li>
+                        })}
+                    </ul>
+                </div>
+                <button className={css.btn}>Проверить почту</button>
             </form>
             <Alerts
                 alerts={alerts}
